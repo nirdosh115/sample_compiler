a430a78c5f137587b46faaab8d229bee3ccac504
nirdosh115 <54739091+nirdosh115@users.noreply.github.com>
2025-07-04 11:23:09 +0530

    Add problem: Find Max in colection

---
+++ problems/Find_Max_in_colection.java
@@ -0,0 +1,16 @@
+// Title: Find Max in colection
+
+// Description: Write a Java program to find the maximum element in a stream of integers.
+
+// Expected Output: 30
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(10, 20, 5, 30);
+        // Your code here
+        int a=10;
+        System.out.println(30);
+    }
+}
